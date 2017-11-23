04d7365feaa9af0e4f011c45a4762f14abc43529
Deividson Muller Goulart <dmg.speed9@gmail.com>
2017-11-23 00:03:42 -0200

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TemperatureMonitorApp from './src/containers/temperatureMonitorApp'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <TemperatureMonitorApp/>
+    )
+  }
+}
