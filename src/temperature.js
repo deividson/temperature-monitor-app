8a77f3f544b1699b9a55fdc62f01b02981a20784
Rafael Eyng <rafaeleyng@gmail.com>
2017-11-25 17:54:29 -0200

    implement last temperature by sensor id

--- src/temperature.js
+++ src/temperature.js
@@ -1,107 +1,156 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  StatusBar,
+} from 'react-native'
 
-import axios from 'axios'
 import { API_URL } from 'react-native-dotenv'
+import axios from 'axios'
+
+const STATUS = {
+  EMPTY: 'EMPTY',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+}
 
 export default class Temperature extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.getLastTemperature = this.getLastTemperature.bind(this)
+  static navigationOptions = {
+    drawerLabel: 'Temperature Monitor'
+  }
 
-        this.state = { 
-          temperature: '',
-          timestamp: '',
-          formattedDate: '',
-          message: ''
+  state = {
+    sensorId: null,
+    status: STATUS.EMPTY,
+    data: null,
+  }
+
+  getData = () => {
+    const { sensorId } = this.state
+
+    axios.get(`${API_URL}/measurements/last/${sensorId}`)
+      .then(res => {
+        const { data } = res.data
+        if (!data) {
+          throw new Error(`No data for sensor ${sensorId}`)
         }
+        return data
+      })
+      .then(data => this.setState({ status: STATUS.SUCCESS, data }))
+      .catch((err) => {
+        console.log('Error while getting data', err)
+        this.setState({ status: STATUS.ERROR })
+      })
+  }
+
+  handleChangeText = (sensorId) => {
+    this.setState({ sensorId, status: STATUS.EMPTY })
+  }
+
+  handlePressButton = () => {
+    if (!this.state.sensorId) {
+      return
+    }
+    this.getData()
+  }
+
+  renderResult() {
+    const { temperature, timestamp, sensorId } = this.state.data
+
+    return (
+      <View style={styles.containerTitle} >
+        <Text>Last temperature for sensor {sensorId}</Text>
+        <Text
+          style={styles.bigText}
+          onPress={this.getData}
+        >
+          {temperature}
+        </Text>
+        <Text style={styles.subtitleText}>{timestamp}</Text>
+      </View>
+    )
+  }
+
+  renderPlaceholder() {
+    return (
+      <View style={styles.containerInput}>
+        <Text>Fill the sensor ID to get the last temperature</Text>
+      </View>
+    )
+  }
+
+  renderError() {
+    return (
+      <View style={styles.containerInput}>
+        <Text>Couldn't get data for sensor {this.state.sensorId}</Text>
+      </View>
+    )
+  }
+
+  render() {
+    const { status } = this.state
+
+    let content
+    if (status === STATUS.EMPTY) {
+      content = this.renderPlaceholder()
+    } else if (status === STATUS.SUCCESS) {
+      content = this.renderResult()
+    } else if (status === STATUS.ERROR) {
+      content = this.renderError()
     }
 
-    componentDidMount() {
-        this.getLastTemperature()        
-    }
-
-    static navigationOptions = {
-        drawerLabel: 'Temperature Monitor'
-    }
-
-    getLastTemperature() {
-        this.setData('-', 'loading...')
-
-        axios.get(`${API_URL}/measurements/last/1`).then(res => {
-          
-            this.setData(
-                res.data.data ? res.data.data.temperature : null,
-                res.data.data ? res.data.data.timestamp : null,
-                res.data.message
-            )
-            console.log('getLastTemperature', res.data)
-        })
-    }
-
-    setData(temperature = '', timestamp = '', message = '') {
-        this.setState({...this.state,
-            temperature: isNaN(parseFloat(temperature)) ? temperature : `${temperature}${String.fromCharCode(176)}`,
-            timestamp: timestamp,
-            formattedDate: new Date(timestamp) instanceof Date ? `on ${new Date(timestamp).toLocaleString()}` : timestamp,
-            message: message
-          })
-    }
-
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-            <View style={styles.container}>
-                <StatusBar barStyle="dark-content" hidden={true}/>
-            
-                <View style={styles.containerTitle} >
-                    <Text style={styles.bigText}
-                        onPress={this.getLastTemperature}>
-                        {this.state.temperature ? this.state.temperature : '-'}
-                    </Text>
-                </View>
-
-                <View style={{
-                        flex: 1,
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                    }}>
-                    <View style={styles.subtitleBlock}>
-                        <Text style={styles.subtitleText}>{this.state.formattedDate}</Text>
-                    </View>
-                    <View style={styles.subtitleBlock} >
-                        <Text>Message: {this.state.message}</Text>
-                    </View>
-                </View>
-          </View>            
-        )
-    }
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" hidden={true}/>
+        <View style={styles.containerInput}>
+          <TextInput
+            placeholder={'Enter the sensor id'}
+            style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={this.handleChangeText}
+            value={this.state.sensorId}
+          />
+          <Button
+            style={{width: 200}}
+            onPress={this.handlePressButton}
+            title="Get last temperature"
+          />
+        </View>
+        {content}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }, 
-    containerTitle: {
-        flex: 3,        
-        alignItems: 'center',
-        justifyContent: 'center',       
-        // backgroundColor: 'powderblue',
-    },
-    subtitleBlock: {
-        height: 50
-    },
-    bigText: {
-        fontSize: 70,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    }, 
-    subtitleText: {
-        textAlign: 'center',        
-    }
-  })
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  containerInput: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  containerTitle: {
+    flex: 2,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // backgroundColor: 'powderblue',
+  },
+  subtitleBlock: {
+    height: 50
+  },
+  bigText: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitleText: {
+    textAlign: 'center',
+  }
+})
